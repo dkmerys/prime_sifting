f8bd6014e8de92da9183ea935cfd0064d63e7822
Mark Gridley <mark_gridley@yahoo.com>
2020-05-07 16:06:34 -0700

    write logic for nimbers divisible by 2, 3, 5, and 7, all tests PASS

--- lib/prime_sifting.rb
+++ lib/prime_sifting.rb
@@ -1,23 +1,32 @@
 require 'pry'
 
 class Prime
-  attr_reader(:user_number, :sieve_numbers)
+  attr_reader(:user_number, :number_array, :no_2s_array, :no_3s_array, :no_primes_array,)
   def initialize (user_number)
     @user_number = user_number
-    @sieve_number = sieve_number
+    @number_array = number_array
+    @no_primes_array = no_primes_array
+    
+
   end
 
-  def number_range()
-    number = 2..@user_number
-    # .map{ |n| Prime.prime?(n)}
+  def number_array
+    @number_array = (2..@user_number).to_a 
   end
 
-  def remove_2s
-    number_range.reject { |r| r.even?}
+  def no_2s
+    @no_2s_array = number_array.delete_if {|n| n.even? && n > 2}
+  end
+
+  def no_3s
+    @no_3s_array = no_2s.delete_if {|n| n % 3 == 0 && n > 3}
+  end
+
+  def no_5s
+    @no_5s_array = no_3s.delete_if {|n| n % 5 == 0 && n > 5}
   end
   
-  def sieve_number
-    @sieve_number = [1, 2, 3, 5, 7]
+  def no_7s
+    @no_7s_array = no_5s.delete_if {|n| n % 7 == 0 && n > 7}
   end
-
 end
--- spec/prime_sifting_spec.rb
+++ spec/prime_sifting_spec.rb
@@ -1,13 +1,25 @@
 require('rspec')
 require('prime_sifting')
 
-describe ('prime-number') do
-  it("returns an array from 2 to the inputted number") do
+describe ('Prime') do
+  it("returns an arry from 2 to the user-inputted number") do
     prime = Prime.new(12)
-    expect(prime.number_range).to(eq(2..12))
+    expect(prime.number_array).to(eq([2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]))
   end
-  it("returns an array containing every other item from the range") do
+  it("returns an array containing numbers larger than 2 but not divisible by 2") do
     prime = Prime.new(12)
-    expect(prime.remove_2s).to(eq([3, 5, 7, 9, 11]))
+    expect(prime.no_2s).to(eq([2, 3, 5, 7, 9, 11]))
   end
-end
+  it("returns an array containing numbers larger than 2 but not divisible by 2 or 3") do
+    prime = Prime.new(12)
+    expect(prime.no_3s).to(eq([2, 3, 5, 7, 11]))
+  end
+  it("returns an array containing numbers larger than 2 but not divisible by 2, 3, or 5") do
+    prime = Prime.new(20)
+    expect(prime.no_5s).to(eq([2, 3, 5, 7, 11, 13, 17, 19]))
+  end
+  it("returns an array containing numbers larger than 2 but not divisible by 2, 3, 5, or 7") do
+    prime = Prime.new(25)
+    expect(prime.no_7s).to(eq([2, 3, 5, 7, 11, 13, 17, 19, 23]))
+  end
+end 
